fix memory leakage in test.py

test.py
@@ -1,13 +1,17 @@
+
+
 import csv
 import logging
 import os
 from argparse import ArgumentParser
 from collections import defaultdict
 
+import einops
 import numpy as np
 import torch
 from monai.inferers import SlidingWindowInferer
 from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image
 from torchvision.transforms.v2.functional import to_pil_image
 from torchvision.utils import make_grid
 from tqdm import tqdm
@@ -90,18 +94,13 @@ def test(args):
 
     os.makedirs(args.scores_dir, exist_ok=True)
     model_prediction_dir = os.path.join(args.scores_dir, model_name, "predictions")
-    model_cam_dir = os.path.join(args.scores_dir, model_name, "gradcam")
 
     os.makedirs(model_prediction_dir, exist_ok=True)
 
-    if cam_model is not None:
-        os.makedirs(model_cam_dir, exist_ok=True)
-
     logger.info(f"Predictions will be saved to {model_prediction_dir}")
 
-    per_size_scores = {size: defaultdict(list) for size in LESION_SIZES}
-    local_metrics = {size: defaultdict(float) for size in LESION_SIZES}
-    global_metrics = defaultdict(list)
+    per_size_scores = {size: defaultdict(lambda: {"sum": 0.0, "count": 0}) for size in LESION_SIZES}
+    global_scores = defaultdict(lambda: {"sum": 0.0, "count": 0})
 
     roi_size = (args.subvolume_dim, *args.resize_to) if args.resize_to else (args.subvolume_dim, *args.scan_dim[3:])
     inferer = SlidingWindowInferer(
@@ -130,64 +129,68 @@ def test(args):
         grayscale_cam = torch.zeros_like(preds[0])
 
         if cam_model is not None:
-            grayscale_cam = cam_model(scans, [SemanticSegmentationTarget(mask) for mask in masks],
-                                      eigen_smooth=False)  # noqa
+            targets = [SemanticSegmentationTarget(mask) for mask in masks]
+            grayscale_cam = cam_model(scans, targets, eigen_smooth=False)  # noqa
             grayscale_cam = torch.from_numpy(grayscale_cam)
+            del targets
 
         predictions_until_now = batch_idx * args.batch_size
         for i in range(preds.shape[0]):
             if args.n_predictions > predictions_until_now:
-
                 pred_dir = os.path.join(model_prediction_dir, f"scan_{predictions_until_now + i}")  # noqa
-                grad_dir = os.path.join(model_cam_dir, f"scan_{predictions_until_now + i}")  # noqa
-
                 os.makedirs(pred_dir, exist_ok=True)
-
-                if cam_model is not None:
-                    os.makedirs(grad_dir, exist_ok=True)
 
             for slice_idx in range(preds.shape[-3]):
                 scan_slice = scans[i][0, slice_idx]
                 mask_slice = masks[i][0, slice_idx]
                 pred_slice = preds[i][0, slice_idx]
 
-                gt = generate_overlayed_slice(scan_slice, mask_slice, color=(0, 255, 0), return_tensor=True)
+                gt, scan_slice, _ = generate_overlayed_slice(scan_slice, mask_slice, color=(0, 255, 0), return_tensor=True, return_rgbs=True)
                 pd = generate_overlayed_slice(scan_slice, pred_slice, color=(255, 0, 0), return_tensor=True)
 
                 # compute per size metrics
                 lesion_size = get_lesion_size_category(mask_slice)
                 scores = compute_metrics(pred_slice, mask_slice, metrics_fns)
+                tp_fp_dict = slice_wise_fp_fn(pred_slice, mask_slice)
+                scores = {**scores, **tp_fp_dict}
 
                 for metric_name, value in scores.items():
-                    per_size_scores[lesion_size][metric_name].append(value)
+                    per_size_scores[lesion_size][metric_name]["sum"] += value
+                    per_size_scores[lesion_size][metric_name]["count"] += 1
                     # exclude from global metrics as free-lesion slices are in majority and could push
                     # the overall scores much higher than they actually are
-                    if lesion_size != 'No Lesion':
-                        global_metrics[metric_name].append(value)
-
-                tp_fp_dict = slice_wise_fp_fn(pred_slice, mask_slice)
-                for key, value in tp_fp_dict.items():
-                    per_size_scores[lesion_size][key].append(value)
-                    if lesion_size != 'No Lesion':
-                        global_metrics[key].append(value)
+                    if lesion_size != "No Lesion":
+                        global_scores[metric_name]["sum"] += value
+                        global_scores[metric_name]["count"] += 1
 
                 if args.n_predictions > predictions_until_now:
-                    grid = make_grid([gt, pd], nrow=2)
-                    to_pil_image(grid).save(os.path.join(pred_dir,
-                                                         f"{lesion_size.replace(' ', '_')}_{predictions_until_now + i}_{slice_idx}.png"))  # noqa
+                    images = [gt, pd]
 
-                    if cam_model:
-                        grid = make_grid([gt, pd, grayscale_cam[i]], nrow=3)
-                        to_pil_image(grid).save(os.path.join(grad_dir,
-                                                             f"{lesion_size.replace(' ', '_')}_{predictions_until_now + i}_{slice_idx}.png"))  # noqa
+                    if cam_model is not None:
+                        cam_overlay = torch.from_numpy(show_cam_on_image(scan_slice / 255,
+                                                                         grayscale_cam[i, slice_idx],
+                                                                         use_rgb=True))
+                        cam_overlay = einops.rearrange(cam_overlay, "h w c -> c h w")
+                        images.append(cam_overlay)
+                        del cam_overlay
+
+                    grid = make_grid(images, nrow=len(images))
+                    to_pil_image(grid).save(os.path.join(pred_dir, f"{lesion_size.replace(' ', '_')}_{predictions_until_now + i}_{slice_idx}.png"))  # noqa
+                    del grid
+
+                # free memory
+                del gt, pd, scan_slice, mask_slice, pred_slice
+                torch.cuda.empty_cache()
 
     logger.info("Inference complete. Computing metrics...")
 
+    local_metrics = {}
     for size, metric_dict in per_size_scores.items():
-        for metric, values in metric_dict.items():
-            local_metrics[size][metric] = float(np.mean(values))
+        local_metrics[size] = {metric: (values["sum"] / values["count"] if values["count"] > 0 else 0.0)
+                               for metric, values in metric_dict.items()}
 
-    global_metrics = {metric: round(float(np.mean(values)), 4) for metric, values in global_metrics.items()}
+    global_metrics = {metric: round(values["sum"] / values["count"], 4) if values["count"] > 0 else 0.0
+                      for metric, values in global_scores.items()}
     global_metrics = {"model_name": model_name, **global_metrics}
 
     scores_path = os.path.join(args.scores_dir, args.scores_file)

utils/utils.py
@@ -167,20 +167,22 @@ def build_metrics(num_classes: int, average: Literal["micro", "macro", "weighted
     }
 
 
-def generate_overlayed_slice(slice, mask_slice, color=(0, 255, 0), return_tensor: bool = False):
-    if slice.min() < 0 or slice.max() > 1:
+def generate_overlayed_slice(scan_slice, mask_slice, color=(0, 255, 0), return_tensor: bool = False, return_rgbs: bool = False):
+    if scan_slice.min() < 0 or scan_slice.max() > 1:
         # normalize scan as RGB conversion requires [0,1] range
-        slice = (slice - slice.min()) / (slice.max() - slice.min())
+        scan_slice = (scan_slice - scan_slice.min()) / (scan_slice.max() - scan_slice.min())
 
-    slice = np.asarray(to_pil_image(slice).convert("RGB"))
+    scan_slice = np.asarray(to_pil_image(scan_slice).convert("RGB"))
     mask_slice = np.asarray(to_pil_image(mask_slice).convert("RGB"))
 
-    overlay = overlay_img(slice, mask_slice, color=color)
+    overlay = overlay_img(scan_slice, mask_slice, color=color)
 
     if return_tensor:
         overlay = torch.from_numpy(overlay)
         overlay = einops.rearrange(overlay, "h w c -> c h w")
-        return overlay
+
+    if return_rgbs:
+        return overlay, scan_slice, mask_slice
     return overlay
 
 
@@ -288,14 +290,8 @@ def slice_wise_fp_fn(prediction: Union[torch.Tensor, np.ndarray],
     fn = ((1 - prediction_flat) * target_flat).sum()
 
     fp_norm = fp.float() / total_voxels
-
-    # no positives in GT
-    if lesion_size == 0:
-        return {"tp": torch.tensor(0.0).item(), "fp": fp_norm.item(), "fn": torch.tensor(0.0)}
-
-    tp_norm = tp.float() / lesion_size
-
     fn_norm = fn.float() / total_voxels
+    tp_norm = tp.float() / lesion_size if lesion_size > 0 else torch.tensor(0.0)
 
     return {"tp": tp_norm.item(), "fp": fp_norm.item(), "fn": fn_norm.item()}
 